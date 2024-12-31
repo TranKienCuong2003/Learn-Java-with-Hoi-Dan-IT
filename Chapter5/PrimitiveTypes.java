e8321e1819866d75de35be1501970d03fcd1014a
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 05:24:34 +0700

    Add PrimitiveTypes.java file

---
+++ Chapter5/PrimitiveTypes.java
@@ -0,0 +1,30 @@
+package Chapter5;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        byte byteValue = 10;
+        System.out.println("byte value: " + byteValue);
+
+        short shortValue = 1000;
+        System.out.println("short value: " + shortValue);
+
+        int intValue = 100000;
+        System.out.println("int value: " + intValue);
+
+        long longValue = 10000000000L;
+        System.out.println("long value: " + longValue);
+
+        float floatValue = 10.5f;
+        System.out.println("float value: " + floatValue);
+
+        double doubleValue = 20.99;
+        System.out.println("double value: " + doubleValue);
+
+        char charValue = 'A';
+        System.out.println("char value: " + charValue);
+
+        boolean booleanValue = true;
+        System.out.println("boolean value: " + booleanValue);
+    }
+}
+
