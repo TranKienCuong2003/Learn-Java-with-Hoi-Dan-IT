a6ba9b7c254ad0e887c1833d7e40fdfef313ba63
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 05:55:39 +0700

    Add SynchronizedExample.java file

---
+++ Chapter11/SynchronizedExample.java
@@ -0,0 +1,44 @@
+package Chapter11;
+
+class Counter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class SynchronizedExample {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+        
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Gia tri count cuoi cung: " + counter.getCount());
+    }
+}
+
