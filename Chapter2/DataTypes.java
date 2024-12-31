4460dfdb1f464110ac353b78773620638264ef69
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 04:45:36 +0700

    Add DataTypes.java file

---
+++ Chapter2/DataTypes.java
@@ -0,0 +1,17 @@
+package Chapter2;
+
+public class DataTypes {
+    static int myNum = 5; // Integer
+    static float myFloatNum = 5.99f; // Floating point number
+    static char myLetter = 'D'; // Character
+    static boolean myBool = true; // Boolean
+    static String myText = "Hello"; // String
+    
+        public static void main(String[] args) {
+            System.out.println(myNum);
+            System.out.println(myFloatNum);
+            System.out.println(myLetter);
+            System.out.println(myBool);
+            System.out.println(myText);
+    }
+}
