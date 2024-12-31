36b133ac23371f4d6012a8f5a90ee0d2094636fc
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 04:48:13 +0700

    Add If.java file

---
+++ Chapter2/If.java
@@ -0,0 +1,10 @@
+package Chapter2;
+
+public class If {
+    public static void main(String[] args) {
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        }
+    }
+}
