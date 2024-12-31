a1bd6d06bc533664fbb2da5de7cc23e818b61a8d
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 04:51:46 +0700

    Add PrimitiveDataTypes.java file

---
+++ Chapter2/PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+package Chapter2;
+
+public class PrimitiveDataTypes {
+    static byte myByte = 100;
+    static short myShort = 5000;
+    static int myInt = 100000;
+    static long myLong = 15000000000L;
+    static float myFloat = 5.75f;
+    static double myDouble = 19.99d;
+    static char myChar = 'A';
+    static boolean myBoolean = true;
+
+    public static void main(String[] args) {
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+    }
+}
