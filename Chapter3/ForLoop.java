e7c5baaa5464f317e73677d680ee619e7c62034a
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 05:07:18 +0700

    Add ForLoop.java file

---
+++ Chapter3/ForLoop.java
@@ -0,0 +1,10 @@
+package Chapter3;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("\nCac so chan tu 0 den 20:");
+        for (int i = 0; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
