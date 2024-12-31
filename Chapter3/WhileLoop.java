1b47c0c72e8afc998913c41b84525f1562add8a3
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 05:08:06 +0700

    Add WhileLoop.java flie

---
+++ Chapter3/WhileLoop.java
@@ -0,0 +1,13 @@
+package Chapter3;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        System.out.println("\nCac so chan tu 0 den 20:");
+        int j = 0;
+        while (j <= 20) {
+            System.out.println(j);
+            j += 2;
+        }
+    }
+}
+
