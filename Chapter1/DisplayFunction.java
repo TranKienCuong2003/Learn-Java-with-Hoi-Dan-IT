663970828024398c99abee08a4d7989bd630cb8d
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 04:36:23 +0700

    Add DisplayFunction.java file

---
+++ Chapter1/DisplayFunction.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+public class DisplayFunction {
+    public static void main(String[] args) {
+        System.out.print("Kien Cuong");
+        System.out.println( );
+
+        System.out.print("Tran Kien Cuong");
+        System.out.println("hoc Java");
+        System.out.printf("duoc %d diem", 10);
+    }
+}
