259b25c43c1de1cf7efcd3203f6355b87836ba14
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 04:39:04 +0700

    Add Variables.java file

---
+++ Chapter1/Variables.java
@@ -0,0 +1,10 @@
+package Chapter1;
+
+public class Variables {
+    public static void main(String[ ] args) {
+        int a = 6;
+        int b = 9;
+        int c = a + b;
+        System.out.println("Sum: " + c);
+        }
+}
